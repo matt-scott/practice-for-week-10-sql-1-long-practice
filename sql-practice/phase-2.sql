d237250053188f61b003f94cf8f15d6b544fcff9
Matt Scott <mattscott251@gmail.com>
2023-11-02 12:20:30 -0500

    completed phase 2

--- sql-practice/phase-2.sql
+++ sql-practice/phase-2.sql
@@ -1,1 +1,23 @@
--- Your code here
+-- Drop tables with existing names
+
+DROP TABLE IF EXISTS customers;
+DROP TABLE IF EXISTS coffee_purchases;
+
+
+-- Create tables
+
+CREATE TABLE customers (
+    id INTEGER PRIMARY KEY,
+    name VARCHAR(40) NOT NULL,
+    phone INTEGER(10) UNIQUE,
+    email VARCHAR(255) UNIQUE,
+    points INTEGER NOT NULL DEFAULT 5,
+    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE coffee_purchases (
+    id INTEGER [pk],
+    is_redeemed BOOLEAN DEFAULT FALSE,
+    ordered_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
